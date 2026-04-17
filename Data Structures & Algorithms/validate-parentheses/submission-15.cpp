98521c41e961d8948941425372d6387c18cf014c
Farhan Pavel <farhanpavel3@gmail.com>
2026-04-17 12:31:07 +0600

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char>st;
+    if(s[0]==']' || s[0]=='}' || s[0]==')'){
+       return false; 
+    }
+     for(int i=0;i<s.size();i++){
+      if(s[i]=='(' || s[i]=='{' || s[i]=='[' ){
+       st.push(s[i]); 
+      }  
+      else if(!st.empty() && st.top()=='[' && s[i]==']'){
+       st.pop(); 
+      }
+      else if(!st.empty() && st.top()=='(' && s[i]==')'){
+       st.pop(); 
+      }
+      else if(!st.empty() && st.top()=='{' && s[i]=='}'){
+       st.pop(); 
+      }
+      else{
+        st.push(s[i]);
+        break;
+      }
+     }
+     if(st.empty()){
+        return true;
+     }
+     return false;   
+    }
+};
