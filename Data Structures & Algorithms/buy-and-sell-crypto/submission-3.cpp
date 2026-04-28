bbf3dbcc60201e64434e612794fdff96ba97dbac
Farhan Pavel <farhanpavel3@gmail.com>
2026-04-28 22:57:45 +0600

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    int left=0,right=0,n=prices.size();
+    int ans=INT_MIN;
+    while(right<n){
+    if(prices[left]>=prices[right]){
+    left=right;
+    } 
+    ans=max(ans,prices[right]-prices[left]);   
+    right++;    
+    }
+    return ans;    
+    }
+};
