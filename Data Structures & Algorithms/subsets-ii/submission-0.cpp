8dd45b21dda6c38a19fc03b7ccf7fb9c00766535
Farhan Pavel <farhanpavel3@gmail.com>
2026-05-01 11:26:55 +0600

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void printSubset(vector<int>&nums,vector<int>&arr,int i){
+    if(i==nums.size()){
+     ans.push_back(arr);
+     return;   
+    }
+   
+    // yes we will take it    
+    arr.push_back(nums[i]);    
+    printSubset(nums,arr,i+1);
+    //no we will not take it
+    while(i+1<nums.size() && nums[i]==nums[i+1]){
+     i++;   
+    }
+    arr.pop_back();
+    printSubset(nums,arr,i+1);
+
+    }
+
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    int i=0;
+    vector<int>arr;
+    sort(nums.begin(),nums.end());
+    printSubset(nums,arr,i);           
+    return ans;
+    }
+};
