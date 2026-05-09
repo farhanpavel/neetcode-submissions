95b2a98e7996db617111c93e45ce24b1352964cc
Farhan Pavel <farhanpavel3@gmail.com>
2026-05-09 14:10:30 +0600

    Add: restore-ip-addresses - submission-3

---
+++ Data Structures & Algorithms/restore-ip-addresses/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string>ans;
+    string store="";
+    void backtrack(string s,string temp,string store ,int count){
+    if(s.size()==0){
+    return;    
+    }
+    if(store.size()<3 &&  store.size() > 0  && stoi(store)>255){
+     return ;   
+    }
+    if(count==3 && s.size()>1 && s[0]=='0'){
+     return;   
+    }
+    if(count==3){ 
+    if(s.size()>3) return; 
+    if(stoi(s)>255) return;
+    ans.push_back(temp+s);    
+    return ;
+    }
+    for(int i=0;i<s.size();i++){
+    store=s.substr(0 ,i+1);
+    if(store.size()>1 && store[0]=='0') break;
+    if(store.size()>=1 && stoi(store)>255) break;
+
+    backtrack(s.substr(i+1), temp+store+'.',store,count+1);
+    }    
+    
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+    string temp="";
+    int count=0;
+    string temp2="";
+    backtrack(s,temp,temp2,count);
+    return ans;
+    }
+};
